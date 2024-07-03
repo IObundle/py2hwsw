bacae2e2554826e3d151a6e63a402de9bb1aaebb
João Vinagre <marquesvinagre@tecnico.ulisboa.pt>
2024-07-03 12:35:21 +0100

    Extend  to wrap combinatory circuits in always(*) block

--- py2hwsw/scripts/iob_comb.py
+++ py2hwsw/scripts/iob_comb.py
@@ -3,14 +3,14 @@ from iob_snippet import iob_snippet
 
 @dataclass
 class iob_comb(iob_snippet):
-    """Class to represent a Verilog combinational circuit in an iob module"""
+    """Class to represent a Verilog combinatory circuit in an iob module"""
 
     def __post_init__(self):
         """Wrap verilog code with the always block"""
         self.verilog_code = f'''\talways @ (*)\n\t\tbegin\n''' + '''\t\t\t''' +  self.verilog_code + '''\t\tend\n'''
 
 def create_comb(core, *args, **kwargs):
-    """Create a Verilog combinational circuit to insert in a given core."""
+    """Create a Verilog combinatory circuit to insert in a given core."""
     # Ensure 'combs' list exists
     core.set_default_attribute("combs", [])
     comb = iob_comb(*args, **kwargs)

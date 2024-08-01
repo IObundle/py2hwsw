28838ef99ab07e32b514320fbccd79ebb5555883
João Vinagre <marquesvinagre@tecnico.ulisboa.pt>
2024-08-01 19:30:10 +0100

    Add FSM example and needed reg modules

---
+++ examples/iob_fsm3/iob_fsm3.py
@@ -0,0 +1,37 @@
+def setup(py_params_dict):
+    attributes_dict = {
+        "original_name": "iob_fsm3",
+        "name": "iob_fsm3",
+        "version": "0.1",
+        "ports": [
+            {
+                "name": "a",
+                "descr": "Output port",
+                "signals": [
+                    {"name": "a", "width": "8", "direction": "output"},
+                ],
+            },
+        ],
+        "fsms": [
+            {
+                "verilog_code": """
+A: a_o = 0;
+
+B: a_o = 1;
+
+a_o = 2;
+
+if(a_o == 0)
+begin
+    pc_nxt = A;
+end
+else
+begin
+    pc_nxt = B;
+end
+"""
+            }
+        ],
+    }
+
+    return attributes_dict

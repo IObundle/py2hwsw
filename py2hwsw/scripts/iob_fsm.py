dff1f09dab89c6ac72a44133e22e6e6a5d6d22ec
João Vinagre <marquesvinagre@tecnico.ulisboa.pt>
2024-08-13 19:06:06 +0100

    Add for loops syntax to fsm; Look for ports list to infer reg

--- py2hwsw/scripts/iob_fsm.py
+++ py2hwsw/scripts/iob_fsm.py
@@ -11,15 +11,30 @@ class iob_fsm(iob_snippet):
         self.states = self.verilog_code.split("\n\n")
         self.state_reg_width = (len(self.states) - 1).bit_length()
         self.state_names = {}
+        _for_loops = {}
         for i, state in enumerate(self.states):
             tag = re.search(r"^\s*(\w+):", state)
             tag = tag.group(1) if tag else None
             if tag:
+                for_loop = re.search(r"for\s*\(([^)]+)\)", state)
+                for_loop = for_loop.group(1) if for_loop else None
+                if for_loop:
+                    init, cond, update = for_loop.split(";")
+                    _for_loops[tag] = {
+                        "init": init,
+                        "cond": cond,
+                        "update": update
+                    }
+                    #remove for loop from state
+                    state = re.sub(r"for\s*\([^)]+\)", "", state)
                 self.state_names[tag] = i
                 self.states[i] = state.replace(f"{tag}:", "")
         for state_name, i in self.state_names.items():
             for j, state in enumerate(self.states):
                 self.states[j] = state.replace(state_name, f"{self.state_reg_width}'b{i:0{self.state_reg_width}b}")
+        for tag, loop in _for_loops.items():
+            self.states[self.state_names[tag]] = f"{loop['init']};\n{self.states[self.state_names[tag]]}"
+            self.states[self.state_names[tag+"_endfor"]] = f"{self.states[self.state_names[tag+'_endfor']]}\n{loop['update']};\nif ({loop['cond']}) begin\npc_nxt = {self.state_reg_width}'b{self.state_names[tag]:0{self.state_reg_width}b};\nend"
         for i, state in enumerate(self.states[:-1]):
             self.states[i] = f"{i}: begin\n{state}\nend"
         self.states[-1] = f"default: begin\n{self.states[-1]}\nend"

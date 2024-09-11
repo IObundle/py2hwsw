31e3ea9b140e6b1bacc36775ac7e04470096aee3
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-09-11 02:04:35 +0100

    wip(csrs): Update method of configuring automatic csrs. No longer use python parameters for auto-csrs. Now the user provides a CSR with the correct "type" ("INTERRUPT", "FIFO_R", "AFIFO_W", ...), and the scripts will replace this one by the complete CSRs for the given type.

---
+++ py2hwsw/lib/hardware/csrs/scripts/interrupts.py
@@ -0,0 +1,55 @@
+interrupt_csrs = [
+    {
+        "name": "status",
+        "type": "R",
+        "n_bits": 32,
+        "rst_val": 0,
+        "log2n_items": 0,
+        "autoreg": True,
+        "descr": "Interrupts status: active (1), inactive (0).",
+    },
+    {
+        "name": "mask",
+        "type": "W",
+        "n_bits": 32,
+        "rst_val": 0,
+        "log2n_items": 0,
+        "autoreg": True,
+        "descr": "Interrupts mask: enable (0), disable (1) for each interrupt.",
+    },
+    {
+        "name": "clear",
+        "type": "W",
+        "n_bits": 32,
+        "rst_val": 0,
+        "log2n_items": 0,
+        "autoreg": True,
+        "descr": "Interrupts clear: clear (1), do not clear (0) for each interrupt.",
+    },
+]
+
+
+def find_and_update_interrupt_csrs(csrs_dict):
+    """Given a dictionary of CSRs, find the interrupt CSRs group and update the dictionary
+    accordingly.
+    User should provide a CSRs of type "INTERRUPT". This CSR will be replaced by interrupt_csrs.
+    """
+    csr_group_ref = None
+    csr_ref = None
+    for csr_group in csrs_dict:
+        for csr in csr_group["regs"]:
+            if csr["type"] == "INTERRUPT":
+                csr_group_ref = csr_group
+                csr_ref = csr
+                break
+        if csr_ref:
+            break
+
+    if not csr_ref:
+        return
+
+    # Add interrupt_csrs to group
+    csr_group_ref["regs"] += interrupt_csrs
+
+    # Remove original csr from csr_group
+    csr_group_ref["regs"].pop(csr_ref)

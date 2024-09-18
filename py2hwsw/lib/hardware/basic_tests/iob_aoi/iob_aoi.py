ea621ebc9def3b2ca547166b1eb03e9642fa9cf6
João Vinagre <marquesvinagre@tecnico.ulisboa.pt>
2024-09-18 12:27:21 +0100

    Change iob_aoi to only use shorthand notation

--- py2hwsw/lib/hardware/basic_tests/iob_aoi/iob_aoi.py
+++ py2hwsw/lib/hardware/basic_tests/iob_aoi/iob_aoi.py
@@ -11,41 +11,22 @@ def setup(py_params_dict):
         "name": "iob_aoi",
         "version": "0.1",
         "ports": [
-            {
-                "name": "a_i",
-                "descr": "Input port",
-                "signals": [
-                    {"name": "a", "width": 1, "direction": "input"},
-                ],
-            },
-            {
-                "name": "b_i",
-                "descr": "Input port",
-                "signals": [
-                    {"name": "b", "width": 1, "direction": "input"},
-                ],
-            },
-            {
-                "name": "c_i",
-                "descr": "Input port",
-                "signals": [
-                    {"name": "c", "width": 1, "direction": "input"},
-                ],
-            },
-            {
-                "name": "d_i",
-                "descr": "Input port",
-                "signals": [
-                    {"name": "d", "width": 1, "direction": "input"},
-                ],
-            },
-            {
-                "name": "y_o",
-                "descr": "Output port",
-                "signals": [
-                    {"name": "y", "width": 1, "direction": "output"},
-                ],
-            },
+            """
+            a_i -s a 1 input
+            'Input port a'
+
+            b_i -s b 1 input
+            'Input port b'
+
+            c_i -s c 1 input
+            'Input port c'
+
+            d_i -s d 1 input
+            'Input port d'
+
+            y_o -s y 1 output
+            'Output port y'
+            """
         ],
         "wires": [
             """
@@ -60,49 +41,31 @@ def setup(py_params_dict):
             """,
         ],
         "blocks": [
-            {
-                "core_name": "iob_and",
-                "instance_name": "iob_and_ab",
-                "parameters": {
-                    "W": 1,
-                },
-                "connect": {
-                    "a_i": "a_i",
-                    "b_i": "b_i",
-                    "y_o": "and_ab_out",
-                },
-            },
             """
+            iob_and iob_and_ab -p W:1 -c
+            a_i:a_i
+            b_i:b_i
+            y_o:and_ab_out
+            'First and gate'
+
             iob_and io_and_cd -p W:1 -c 
             a_i:c_i 
             b_i:d_i 
             y_o:and_cd_out
             'Second and gate'
-            """,
-            {
-                "core_name": "iob_or",
-                "instance_name": "iob_or_abcd",
-                "parameters": {
-                    "W": 1,
-                },
-                "connect": {
-                    "a_i": "and_ab_out",
-                    "b_i": "and_cd_out",
-                    "y_o": "or_out",
-                },
-            },
-            {
-                "core_name": "iob_inv",
-                "instance_name": "iob_inv_out",
-                "parameters": {
-                    "W": 1,
-                },
-                "connect": {
-                    "a_i": "or_out",
-                    "y_o": "y_o",
-                },
-            },
-        ],
+
+            iob_or iob_or_abcd -p W:1 -c
+            a_i:and_ab_out
+            b_i:and_cd_out
+            y_o:or_out
+            'Or gate'
+
+            iob_inv iob_inv_out -p W:1 -c
+            a_i:or_out
+            y_o:y_o
+            'Inverter'
+            """
+        ]
     }
 
     return attributes_dict

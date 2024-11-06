c42850742d6da75dcabd6fc978003d59c66b4564
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-11-06 01:37:54 +0000

    feat(doc_only): Add doc_only attribute to confs and csr_groups.

--- py2hwsw/lib/hardware/iob_csrs/scripts/csr_gen.py
+++ py2hwsw/lib/hardware/iob_csrs/scripts/csr_gen.py
@@ -115,11 +115,15 @@ class csr_gen:
             # a or b is a string
             return f"(({a} > {b}) ? {a} : {b})"
 
-    def get_reg_table(self, regs, rw_overlap, autoaddr):
+    def get_reg_table(self, csrs, rw_overlap, autoaddr):
         # Create reg table
         reg_table = []
-        for i_regs in regs:
-            reg_table += i_regs.regs
+        for csr_group in csrs:
+            # If csr_group has 'doc_only' attribute set to True, skip it
+            if csr_group.doc_only:
+                continue
+
+            reg_table += csr_group.regs
 
         return self.compute_addr(reg_table, rw_overlap, autoaddr)
 
@@ -1458,24 +1462,25 @@ class csr_gen:
         csrs_file.close()
 
     # Generate TeX tables of registers
-    # regs: list of tables containing registers, as defined in <corename>_setup.py
+    # csrs: list of tables containing registers, as defined in <corename>_setup.py
     # regs_with_addr: list of all registers, where 'addr' field has already been computed
     # out_dir: output directory
     @classmethod
-    def generate_regs_tex(self, regs, regs_with_addr, out_dir):
+    def generate_regs_tex(self, csrs, regs_with_addr, out_dir):
         os.makedirs(out_dir, exist_ok=True)
         # Create csrs.tex file
-        self.generate_csrs_tex(regs, out_dir)
+        self.generate_csrs_tex(csrs, out_dir)
 
-        for table in regs:
+        for csr_group in csrs:
             tex_table = []
-            for reg in table.regs:
+            for reg in csr_group.regs:
+                addr = "None"
                 # Find address of matching register in regs_with_addr list
-                addr = next(
-                    register.addr
-                    for register in regs_with_addr
-                    if register.name == reg.name
-                )
+                for reg_with_addr in regs_with_addr:
+                    if reg_with_addr.name == reg.name:
+                        addr = reg_with_addr.addr
+                        break
+
                 tex_table.append(
                     [
                         reg.name,
@@ -1487,4 +1492,4 @@ class csr_gen:
                     ]
                 )
 
-            write_table(f"{out_dir}/{table.name}_csrs", tex_table)
+            write_table(f"{out_dir}/{csr_group.name}_csrs", tex_table)

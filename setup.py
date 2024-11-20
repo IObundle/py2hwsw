341cf63bbd6966ee56628f8b839d2e6285f6287c
Artur Nóbrega <arturnobrega1999@gmail.com>
2024-11-20 18:10:27 +0000

    fix(pip): Create a shortHash.tex file during pip package installation. Py2hwsw cannot use git commands to generate short hash in a pip installation because the pip install directory is not a git repository.
    To resolve this, the shortHash.tex file is created during the pip
    package install process, and then py2hwsw only copies this file to
    the build directory.

--- setup.py
+++ setup.py
@@ -4,6 +4,34 @@
 
 from setuptools import setup
 from py2hwsw.scripts.py2hwsw_version import PY2HWSW_VERSION
+import os
+import subprocess
+from setuptools.command.install import install
+
+
+class CreateShortHashFile(install):
+    """Create a shortHash.tex file in the package directory"""
+
+    def run(self):
+        # Get the current Git commit short hash
+        try:
+            short_hash = subprocess.check_output(
+                ["git", "rev-parse", "--short", "HEAD"], universal_newlines=True
+            ).strip()
+        except (subprocess.CalledProcessError, OSError):
+            short_hash = "unknown"
+
+        # Create the shortHash.tex file in the package directory
+        package_dir = os.path.join(os.path.dirname(__file__), "build/lib/py2hwsw")
+
+        short_hash_file = os.path.join(package_dir, "shortHash.tex")
+        with open(short_hash_file, "w") as f:
+            f.write(short_hash)
+
+        print(f"Created shortHash.tex file with Git commit short hash: {short_hash}")
+
+        install.run(self)
+
 
 setup(
     name="py2hwsw",
@@ -18,4 +46,7 @@ setup(
     package_data={"py2hwsw": ["**/*", ".*"]},
     include_package_data=True,
     zip_safe=False,
+    cmdclass={
+        "install": CreateShortHashFile,
+    },
 )

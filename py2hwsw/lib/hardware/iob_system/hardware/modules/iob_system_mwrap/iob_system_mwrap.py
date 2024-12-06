fb2528f99542a1aaa5bbab912b3db26223efa5b2
João Vinagre <marquesvinagre@tecnico.ulisboa.pt>
2024-12-06 11:34:58 +0000

    Update iob_system_mwrap to use new interfaces

--- py2hwsw/lib/hardware/iob_system/hardware/modules/iob_system_mwrap/iob_system_mwrap.py
+++ py2hwsw/lib/hardware/iob_system/hardware/modules/iob_system_mwrap/iob_system_mwrap.py
@@ -74,8 +74,7 @@ def setup(py_params_dict):
                 "HEXFILE": '{BOOT_HEXFILE, ".hex"}',
             },
             "connect": {
-                "clk_i": "clk",
-                "rom_if_io": "rom_bus_io",
+                "rom_sp_s": "rom_bus_m",
             },
         },
         # Internal memory

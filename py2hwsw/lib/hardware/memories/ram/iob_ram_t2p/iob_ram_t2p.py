cd002cbe11a7f18f22e7aa7746e5cffff37387c9
PMiranda <pmiranda.iobundle@gmail.com>
2024-12-27 19:02:37 +0000

    fix(mem): generate if for RAMS when HEXFILE="none" - addresses IObundle/py2hwsw#95

--- py2hwsw/lib/hardware/memories/ram/iob_ram_t2p/iob_ram_t2p.py
+++ py2hwsw/lib/hardware/memories/ram/iob_ram_t2p/iob_ram_t2p.py
@@ -102,19 +102,20 @@ def setup(py_params_dict):
         "snippets": [
             {
                 "verilog_code": """
-            
 
 
+
+   localparam INIT_RAM = (MEM_INIT_FILE_INT != "none") ? 1 : 0;
    // Declare the RAM
    reg [DATA_W-1:0] mem    [(2**ADDR_W)-1:0];
 
    reg [DATA_W-1:0] r_data;
    // Initialize the RAM
-   initial begin
-       if (MEM_INIT_FILE_INT != "none") begin
-           $readmemh(MEM_INIT_FILE_INT, mem, 0, (2 ** ADDR_W) - 1);
+   generate
+       if (INIT_RAM) begin : mem_init
+           initial $readmemh(MEM_INIT_FILE_INT, mem, 0, (2 ** ADDR_W) - 1);
        end
-   end
+   endgenerate
 
    //read port
    always @(posedge clk_i) begin

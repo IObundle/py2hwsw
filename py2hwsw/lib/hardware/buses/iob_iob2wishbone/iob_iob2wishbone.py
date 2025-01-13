66da2528755d28969c978ff51c3a5ada3a167e05
Andre Nobrega <29956896+And-Nob@users.noreply.github.com>
2025-01-13 16:39:26 +0000

    Fixes iob2wishbone

--- py2hwsw/lib/hardware/buses/iob_iob2wishbone/iob_iob2wishbone.py
+++ py2hwsw/lib/hardware/buses/iob_iob2wishbone/iob_iob2wishbone.py
@@ -6,11 +6,19 @@
 def setup(py_params_dict):
     attributes_dict = {
         "version": "0.1",
-        "generate_hw": False,
+        "ports": [
+            {
+                "name": "clk_en_rst_s",
+                "signals": {
+                    "type": "clk_en_rst",
+                },
+                "descr": "Clock, clock enable and reset",
+            },
+        ],
         "subblocks": [
             {
                 "core_name": "iob_reg_re",
-                "instance_name": "iob_reg_re_inst",
+                "instantiate": False,
             },
         ],
     }

61a19212582f486c6ed59ca145b25ef40a3b8c3e
Artur Nóbrega <arturnobrega1999@gmail.com>
2025-02-06 07:07:03 +0000

    feat(license): Add missing SPDX license headers Add missing headers in `.cpp` files.

--- py2hwsw/lib/hardware/buses/iob_tasks/hardware/src/iob_tasks.cpp
+++ py2hwsw/lib/hardware/buses/iob_tasks/hardware/src/iob_tasks.cpp
@@ -1,6 +1,8 @@
-// SPDX-FileCopyrightText: 2025 IObundle
-//
-// SPDX-License-Identifier: MIT
+/*
+ * SPDX-FileCopyrightText: 2025 IObundle
+ *
+ * SPDX-License-Identifier: MIT
+ */
 
 #include "iob_tasks.h"
 
--- py2hwsw/lib/iob_system/hardware/simulation/src/iob_system_tb.cpp
+++ py2hwsw/lib/iob_system/hardware/simulation/src/iob_system_tb.cpp
@@ -1,6 +1,8 @@
-// SPDX-FileCopyrightText: 2025 IObundle
-//
-// SPDX-License-Identifier: MIT
+/*
+ * SPDX-FileCopyrightText: 2025 IObundle
+ *
+ * SPDX-License-Identifier: MIT
+ */
 
 #include <stdio.h>
 #include <stdlib.h>

2cccd8db6cae9fa2f579b72e40c586cd0283d6d9
João Vinagre <marquesvinagre@tecnico.ulisboa.pt>
2025-03-06 16:38:28 +0000

    New iob_reg can have all combinations

--- py2hwsw/lib/hardware/registers/iob_reg/iob_reg.py
+++ py2hwsw/lib/hardware/registers/iob_reg/iob_reg.py
@@ -2,15 +2,39 @@
 #
 # SPDX-License-Identifier: MIT
 
-edge = 1
-
 
 def setup(py_params_dict):
-    global edge
-    if "RST_POL" in py_params_dict:
-        edge = py_params_dict["RST_POL"]
+    
+    port_params = py_params_dict["port_params"]
+    assert "clk_s" in port_params, "clk_s port is missing"
+
+    clk_s_params = [x for x in port_params["clk_s"].split("_") if x != ""]
+    if all(["arst", "anrst"] in clk_s_params) or all(["rst", "nrst"] in clk_s_params):
+        raise ValueError("There can only be one type of reset signal for each synchronous and asynchronous reset")
+
+    rst_str = ""
+    en_str = ""
+
+    if "arst" in clk_s_params:
+        sensitivity_list = "posedge clk_i, posedge arst_i"
+    elif "anrst" in clk_s_params:
+        sensitivity_list = "posedge clk_i, negedge anrst_i"
+    else:
+        sensitivity_list = "posedge clk_i"
+
+    if any([x in clk_s_params for x in ["arst", "anrst", "rst", "nrst"]]):
+        rst_con = ' | '.join([f"{x}_i" for x in ["arst", "~anrst", "rst", "~nrst"] if x in clk_s_params])
+        rst_str = f"        if ({rst_con}) begin\n            data_o <= RST_VAL;\n        end"
+
+    if "cke" in clk_s_params or "en" in clk_s_params:
+        en_con = ' & '.join([f"{x}_i" for x in ["cke", "en"] if x in clk_s_params])
+        en_str = f"        {'else ' if rst_str != '' else ''}if ({en_con}) begin\n            data_o <= data_i;\n        end"
+    else:
+        en_str = f"        {'else begin' if rst_str != '' else ''}\n            data_o <= data_i;\n        {'end' if rst_str != '' else ''}"
+
     attributes_dict = {
         "generate_hw": True,
+        "version": "0.1",
         "confs": [
             {
                 "name": "DATA_W",
@@ -34,6 +58,7 @@ def setup(py_params_dict):
                 "name": "clk_en_rst_s",
                 "signals": {
                     "type": "iob_clk",
+                    "params": port_params["clk_s"],
                 },
                 "descr": "Clock, clock enable and reset",
             },
@@ -62,12 +87,9 @@ def setup(py_params_dict):
         "snippets": [
             {
                 "verilog_code": f"""
-    always @(posedge clk_i, {"posedge" if edge else "negedge"} arst_i) begin
-      if (arst_i) begin
-        data_o <= RST_VAL;
-      end else if (cke_i) begin
-        data_o <= data_i;
-      end
+    always @({sensitivity_list}) begin
+{rst_str}
+{en_str}
     end
          """,
             },

b06f77ce9d237dcee8b6d97e549674f2afbd4ce6
João Vinagre <marquesvinagre@tecnico.ulisboa.pt>
2025-03-11 18:43:27 +0000

    Remove None param when merging port_params of iob_clk ports; only merge if different

--- py2hwsw/scripts/iob_core.py
+++ py2hwsw/scripts/iob_core.py
@@ -535,9 +535,10 @@ class iob_core(iob_module, iob_instance):
                     p.interface.type == port.interface.type
                     and p.interface.prefix == port.interface.prefix
                 ):
-                    p.interface.params = "_".join(
-                        filter(None, [p.interface.params, port.interface.params])
-                    )
+                    if p.interface.params != port.interface.params:
+                        p.interface.params = "_".join(
+                            filter(lambda x: x != "None", [p.interface.params, port.interface.params])
+                        )
                     port.connect_external(p, bit_slices=[])
                     return
         instantiator.create_port(name=_name, signals=_signals, descr=port.descr)

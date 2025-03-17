3588bc802bc35efa8575c03fdf95c892b8e71ce1
Artur Nóbrega <arturnobrega1999@gmail.com>
2025-03-17 17:48:26 +0000

    fix(mems): Add missing 'reg' signals in memories

--- py2hwsw/lib/hardware/memories/rom/iob_rom_sp/iob_rom_sp.py
+++ py2hwsw/lib/hardware/memories/rom/iob_rom_sp/iob_rom_sp.py
@@ -54,7 +54,7 @@ def setup(py_params_dict):
         "snippets": [
             {
                 "verilog_code": """
-   localparam INIT_RAM = (MEM_INIT_FILE_INT != "none") ? 1 : 0;
+   localparam INIT_RAM = (MEM_INIT_FILE_INT != "none.hex") ? 1 : 0;
             // Declare the ROM
    reg [DATA_W-1:0] rom[(2**ADDR_W)-1:0];
    reg [DATA_W-1:0] r_data_int;

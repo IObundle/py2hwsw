60feaca7774007618ab59e7508e00a66876f5d88
AndreMerendeira <andre.merendeira@gmail.com>
2025-04-07 15:38:27 +0100

    [iob_fifo2axis] changed ports and params to use py2

--- py2hwsw/lib/hardware/buses/iob_fifo2axis/iob_fifo2axis.py
+++ py2hwsw/lib/hardware/buses/iob_fifo2axis/iob_fifo2axis.py
@@ -6,6 +6,23 @@
 def setup(py_params_dict):
     attributes_dict = {
         "generate_hw": False,
+        "confs": [
+            {
+                "name": "DATA_W",
+                "descr": "Data bus width",
+                "type": "P",
+                "val": "0",
+                "min": "1",
+                "max": "NA",
+            },
+            {
+                "name": "AXIS_LEN_W",
+                "descr": "AXIS length bus width",
+                "type": "P",
+                "min": "1",
+                "max": "NA",
+            },
+        ],
         "ports": [
             {
                 "name": "clk_en_rst_s",
@@ -13,6 +30,82 @@ def setup(py_params_dict):
                     "type": "iob_clk",
                 },
                 "descr": "Clock, clock enable, and reset",
+            },
+            {
+                "name": "rst_i",
+                "descr": "Reset signal",
+                "signals": [
+                    {
+                        "name": "rst_i",
+                        "width": 1,
+                        "descr": "Reset signal",
+                    },
+                ],
+            },
+            {
+                "name": "en_i",
+                "descr": "Enable signal",
+                "signals": [
+                    {
+                        "name": "en_i",
+                        "width": 1,
+                        "descr": "Enable signal",
+                    },
+                ],
+            },
+            {
+                "name": "len_i",
+                "descr": "Length signal",
+                "signals": [
+                    {
+                        "name": "len_i",
+                        "width": "AXIS_LEN_W",
+                        "descr": "Length signal",
+                    },
+                ],
+            },
+            {
+                "name": "level_o",
+                "descr": "Level signal",
+                "signals": [
+                    {
+                        "name": "level_o",
+                        "width": 2,
+                        "descr": "Level signal",
+                        "isvar": True,
+                    },
+                ],
+            },
+            {
+                "name": "fifo_r_io",
+                "descr": "FIFO read interface",
+                "signals": [
+                    {
+                        "name": "fifo_read_o",
+                        "width": 1,
+                        "descr": "FIFO read signal",
+                    },
+                    {
+                        "name": "fifo_rdata_i",
+                        "width": "DATA_W",
+                        "descr": "FIFO read data signal",
+                    },
+                    {
+                        "name": "fifo_empty_i",
+                        "width": 1,
+                        "descr": "FIFO empty signal",
+                    },
+                ],
+            },
+            {
+                "name": "axis_m",
+                "descr": "AXIS master interface",
+                "signals": {
+                    "type": "axis",
+                    "prefix": "m_",
+                    "params": "tlast",
+                    "DATA_W": "DATA_W",
+                },
             },
         ],
         "subblocks": [

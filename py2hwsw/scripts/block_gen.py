f9e92f284e7fc9661388973a43e22bd5d59fba5e
AndreMerendeira <andre.merendeira@gmail.com>
2025-04-17 12:49:10 +0100

    [block_gen] added comments and code cleanup. Fixed bit slice overwrite

--- py2hwsw/scripts/block_gen.py
+++ py2hwsw/scripts/block_gen.py
@@ -129,41 +129,51 @@ def get_instance_port_connections(instance):
     for the given Verilog instance.
     """
     instance_portmap = ""
+
+    # Iterade over all ports of the instance
     for port_idx, port in enumerate(instance.ports):
         # If port has 'doc_only' attribute set to True, skip it
         if port.doc_only:
             continue
 
-        assert (
-            port.e_connect
-        ), f"{iob_colors.FAIL}Port '{port.name}' of instance '{instance.name}' is not connected!{iob_colors.ENDC}"
-        newlinechar = "\n"
+        # Check if there are connections for this instance
+        assert (port.e_connect), f"{iob_colors.FAIL}Port '{port.name}' of instance '{instance.name}' is not connected!{iob_colors.ENDC}"
+
+        # If one of the ports is not a standard inferface, check if the number of signals is the same
         if not port.interface or not port.e_connect.interface:
-            assert len(port.signals) == len(
-                port.e_connect.signals
-            ), f"""{iob_colors.FAIL}Port '{port.name}' of instance '{instance.name}' has different number of signals compared to external connection '{port.e_connect.name}'!
+            newlinechar = "\n"
+            assert len(port.signals) == len(port.e_connect.signals), f"""{iob_colors.FAIL}Port '{port.name}' of instance '{instance.name}' has different number of signals compared to external connection '{port.e_connect.name}'!
 Port '{port.name}' has the following signals:
 {newlinechar.join("- " + get_real_signal(port).name for port in port.signals)}
 
 External connection '{get_real_signal(port.e_connect).name}' has the following signals:
 {newlinechar.join("- " + get_real_signal(port).name for port in port.e_connect.signals)}
-{iob_colors.ENDC}"""
+{iob_colors.ENDC}
+"""
 
         # If port has only non-iob signals, skip it
-        if not any(isinstance(signal, iob_signal) for signal in port.signals):
+        if not any(isinstance(port_signal, iob_signal) for port_signal in port.signals):
             continue
-        instance_portmap += f"        // {port.name} port: {port.descr}\n"
+
+        # If port has a description, add it to the portmap
+        if port.descr and not port.doc_only:
+            instance_portmap += f"        // {port.name} port: {port.descr}\n"
+
         # Connect individual signals
-        for idx, signal in enumerate(port.signals):
-            if not isinstance(signal, iob_signal):
+        for idx, port_signal in enumerate(port.signals):
+            # Skip signals that are not iob_signals
+            if not isinstance(port_signal, iob_signal):
                 continue
-            port_name = signal.name
-            # Connect interface signals by name and not by order
+            port_name = port_signal.name
+
+            # If both ports are standard interfaces, connect by name
             if port.interface and port.e_connect.interface:
+                # Remove prefix and suffix from port name
                 port_name = port_name.replace(port.interface.prefix, "", 1)[:-2]
                 for e_signal in port.e_connect.signals:
                     real_e_signal = get_real_signal(e_signal)
                     e_signal_name = real_e_signal.name
+                    # Remove prefix and suffix from external signal name
                     if e_signal_name[-2:] in ["_o", "_i"]:
                         e_signal_name = e_signal_name[:-2]
                     e_signal_name = e_signal_name.replace(
@@ -171,23 +181,36 @@ External connection '{get_real_signal(port.e_connect).name}' has the following s
                     )
                     if e_signal_name == port_name:
                         e_signal_name = real_e_signal.name
-                        port_name = signal.name
+                        port_name = port_signal.name
                         break
-                port_name = signal.name
+                port_name = port_signal.name
             else:
+                # If both ports are not standard interfaces, connect by index
                 real_e_signal = get_real_signal(port.e_connect.signals[idx])
                 e_signal_name = real_e_signal.name
 
+            # If the signal is a bit slice, get the name of the bit slice
+            # and the name of the port (this overwrites the previous connection)
             for bit_slice in port.e_connect_bit_slices:
-                if signal.name in bit_slice:
+                if e_signal_name in bit_slice:
                     # Connection is not a bit_slice
-                    if f"{signal.name}:" not in bit_slice:
-                        e_signal_name = bit_slice
-                    # Connection is a bit slice
-                    else:
+                    if f"{port_signal.name}:" in bit_slice:
                         e_signal_name = bit_slice.split(":")[1]
-                        port_name = signal.name
+                        port_name = port_signal.name
+                    else:
+                    # Connection is a bit slice
+                        e_signal_name = bit_slice
                     break
+                elif port_signal.name in bit_slice:
+                    # Connection is not a bit_slice
+                    if f"{port_signal.name}:" in bit_slice:
+                        e_signal_name = bit_slice.split(":")[1]
+                        port_name = port_signal.name
+                    else:
+                    # Connection is a bit slice
+                        e_signal_name = bit_slice
+                    break
+
             instance_portmap += f"        .{port_name}({e_signal_name}),\n"
 
     instance_portmap = instance_portmap[:-2] + "\n"  # Remove last comma

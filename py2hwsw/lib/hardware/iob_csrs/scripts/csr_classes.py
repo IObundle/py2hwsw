76e2239b2c10b9f2d4d0a75aa317fcb6803c49ac
Artur Nóbrega <arturnobrega1999@gmail.com>
2025-04-28 20:59:44 +0100

    wip(csrs): Add csr field class and checks Related to https://github.com/IObundle/py2hwsw/issues/242 and https://github.com/IObundle/py2hwsw/issues/234

--- py2hwsw/lib/hardware/iob_csrs/scripts/csr_classes.py
+++ py2hwsw/lib/hardware/iob_csrs/scripts/csr_classes.py
@@ -24,6 +24,9 @@ class iob_csr:
     internal_use: bool = False
     # List of configurations that use this CSR (used for documentation)
     doc_conf_list: list or None = None
+    volatile: bool = True
+    # Bit fields
+    fields: list or None = None
 
     def __post_init__(self):
         if not self.name:
@@ -31,6 +34,63 @@ class iob_csr:
 
         if self.type not in ["R", "W", "RW"]:
             fail_with_msg(f"Invalid CSR type: '{self.type}'", ValueError)
+
+        # try to convert n_bits to int
+        try:
+            self.n_bits = int(self.n_bits)
+        except ValueError:
+            pass
+
+        if not self.fields:
+            self.fields = [
+                csr_field(name=self.name, type=self.type, base_bit=0, width=self.n_bits)
+            ]
+            # fail_with_msg(f"CSR '{self.name}' has no bit fields", ValueError)
+
+        # Don't try to manage fields, if n_bits is not integer (likely contains parameters)
+        if type(self.n_bits) is not int:
+            return
+
+        # List of csr bits. Value corresponds to index of associated field. -1 means free.
+        used_bits = [-1 for i in range(self.n_bits)]
+        # Check if any fields overlap
+        for idx, _csr_field in enumerate(self.fields):
+            for bit in range(
+                _csr_field.base_bit, _csr_field.base_bit + _csr_field.width
+            ):
+                if used_bits[bit] > -1:
+                    fail_with_msg(
+                        f"CSR '{self.name}' has overlapping fields: '{_csr_field.name}' and '{self.fields[used_bits[bit]].name}'.",
+                        ValueError,
+                    )
+                used_bits[bit] = idx
+
+        # Automatically create RSVD fields for unused bits
+        width = 0
+        for idx, bit in enumerate(used_bits):
+            if bit < 0:
+                width += 1
+            elif width > 0:
+                self.fields.append(
+                    csr_field(name="RSVD", type="R", base_bit=idx - width, width=width)
+                )
+                width = 0
+
+
+@dataclass
+class csr_field:
+    name: str = ""
+    type: str = ""
+    base_bit: int = 0
+    width: int = 1
+    volatile: bool = True
+
+    def __post_init__(self):
+        if not self.name:
+            fail_with_msg("CSR field name is not set", ValueError)
+
+        if self.type not in ["R", "W", "RW"]:
+            fail_with_msg(f"Invalid CSR field type: '{self.type}'", ValueError)
 
 
 def fail_with_msg(msg, exception_type=Exception):

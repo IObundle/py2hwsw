8ab264e154ebf5ceb6607d855c19e1bba79cfc92
PMiranda <pmiranda.iobundle@gmail.com>
2025-05-08 16:48:20 +0100

    feat(dma): initial working vutb

--- py2hwsw/lib/peripherals/iob_dma/software/src/iob_core_tb.c
+++ py2hwsw/lib/peripherals/iob_dma/software/src/iob_core_tb.c
@@ -6,8 +6,12 @@
 
 #include "iob_axistream_in_csrs.h"
 #include "iob_axistream_out_csrs.h"
+#include "iob_dma.h"
 #include "iob_dma_csrs.h"
+
 #include <stdio.h>
+
+#define NWORDS 256
 
 int iob_core_tb() {
 
@@ -27,19 +31,88 @@ int iob_core_tb() {
   // dma connected to third master of split
   iob_dma_csrs_init_baseaddr(2 << 5);
 
-  unsigned int version;
-  int i;
+  unsigned int version, major, minor;
+  uint32_t i, word;
 
   // Check versions
-  version = iob_axistream_in_csrs_get_version();
-  printf("AXIS IN Version is %d\n", version);
-  version = iob_axistream_out_csrs_get_version();
-  printf("AXIS OUT Version is %d\n", version);
   // read version 20 times to burn time
   for (i = 0; i < 20; i++) {
+
     version = iob_dma_csrs_get_version();
   }
-  printf("DMA Version is %d\n", version);
+  major = version >> 16;
+  minor = version & 0xFF;
+  printf("DMA Version is %x.%x\n", major, minor);
+
+  // 1. CPU -> AXIS OUT -> AXIS IN data transfer
+  printf("1.1. Configure AXIStream IN\n");
+  iob_axistream_in_csrs_set_soft_reset(1);
+  iob_axistream_in_csrs_set_soft_reset(0);
+  iob_axistream_in_csrs_set_mode(1);
+  iob_axistream_in_csrs_set_enable(1);
+
+  printf("1.2. Configure AXIStream OUT\n");
+  iob_axistream_out_csrs_set_soft_reset(1);
+  iob_axistream_out_csrs_set_soft_reset(0);
+  iob_axistream_out_csrs_set_mode(0);
+  iob_axistream_out_csrs_set_nwords(NWORDS);
+  iob_axistream_out_csrs_set_enable(1);
+
+  printf("1.3. Write data to AXIStream OUT\n");
+
+  // write data loop
+  for (i = 0; i < NWORDS; i = i + 1) {
+    iob_axistream_out_csrs_set_data(i);
+  }
+
+  // wait for data in AXIS IN
+  while (iob_axistream_in_csrs_get_nwords() < NWORDS)
+    ;
+  // 2. Configure AXIS IN -> DMA -> AXI RAM write operation
+  printf("2.1. Configure DMA write transfer\n");
+  dma_write_transfer(0, NWORDS);
+
+  // 3. Wait for DMA transfer complete
+  printf("3. Wait for DMA write transfer complete...");
+  while (dma_write_busy())
+    ;
+  printf("done!\n");
+
+  // 4. Configure AXIS OUT <- DMA <- AXI RAM read operation
+  printf("4.1. Configure AXIStream IN\n");
+  iob_axistream_in_csrs_set_soft_reset(1);
+  iob_axistream_in_csrs_set_soft_reset(0);
+  iob_axistream_in_csrs_set_mode(0);
+  iob_axistream_in_csrs_set_enable(1);
+
+  printf("4.2. Configure AXIStream OUT\n");
+  iob_axistream_out_csrs_set_soft_reset(1);
+  iob_axistream_out_csrs_set_soft_reset(0);
+  iob_axistream_out_csrs_set_mode(1);
+  iob_axistream_out_csrs_set_nwords(NWORDS);
+  iob_axistream_out_csrs_set_enable(1);
+
+  printf("4.3. Configure DMA read transfer\n");
+  dma_read_transfer(0, NWORDS);
+
+  // 5. Wait for DMA transfer complete
+  printf("3. Wait for DMA read transfer complete...");
+  while (dma_read_busy())
+    ;
+  printf("done!\n");
+
+  // 6. Read data from AXIS IN and validate
+  printf("6. Read data from AXIStream IN\n");
+  // read data loop
+  for (i = 0; i < NWORDS; i = i + 1) {
+    word = iob_axistream_in_csrs_get_data();
+
+    // check data
+    if (word != i) {
+      printf("Error: expected %d, got %d\n", i, word);
+      failed = failed + 1;
+    }
+  }
 
   printf("DMA test complete.\n");
   return failed;
--- py2hwsw/lib/peripherals/iob_dma/software/sw_build.mk
+++ py2hwsw/lib/peripherals/iob_dma/software/sw_build.mk
@@ -7,3 +7,4 @@ TB_INCLUDES=-I./src -I./simulation/src
 CSRS=./src/iob_dma_csrs.c
 CSRS+=./simulation/src/iob_axistream_in_csrs.c
 CSRS+=./simulation/src/iob_axistream_out_csrs.c
+CSRS+=./src/iob_dma.c

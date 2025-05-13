84f30c0a42eef4993ff32a36f3dc6fb9bfef154f
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2025-05-13 23:44:39 +0100

    comb lhs bug

---
+++ py2hwsw/lib/peripherals/iob_axil_macc/iob_axil_macc.py
@@ -0,0 +1,153 @@
+# SPDX-FileCopyrightText: 2025 IObundle
+#
+# SPDX-License-Identifier: MIT
+
+
+def setup(py_params_dict):
+    CSR_IF = py_params_dict["csr_if"] if "csr_if" in py_params_dict else "iob"
+    NAME = py_params_dict["name"] if "name" in py_params_dict else "iob_axil_macc"
+    attributes_dict = {
+        "name": NAME,
+        "generate_hw": True,
+        "board_list": ["iob_cyclonev_gt_dk", "iob_aes_ku040_db_g"],
+        "description": "The IObundle AXIL_MACC is a RISC-V-based Peripheral written in Verilog, which users can download for free, modify, simulate and implement in FPGA or ASIC. It is written in Verilog and includes a C software driver. The IObundle AXIL_MACC is a very compact IP that works at high clock rates if needed. It supports full-duplex operation and a configurable baud rate. The IObundle AXIL_MACC has a fixed configuration for the Start and Stop bits. More flexible licensable commercial versions are available upon request.",
+        "confs": [
+            {
+                "name": "DATA_W",
+                "type": "P",
+                "val": "32",
+                "min": "NA",
+                "max": "NA",
+                "descr": "Data bus width.",
+            },
+        ],
+        "ports": [
+            {
+                "name": "clk_en_rst_s",
+                "signals": {
+                    "type": "iob_clk",
+                },
+                "descr": "Clock, clock enable and reset",
+            },
+        ],
+        "wires": [
+            {
+                "name": "en",
+                "descr": "Enable",
+                "signals": [
+                    {"name": "en", "width": 1},
+                ],
+            },
+            {
+                "name": "enp",
+                "descr": "Enable",
+                "signals": [
+                    {"name": "enp", "width": 1},
+                ],
+            },
+            {
+                "name": "load",
+                "descr": "Load",
+                "signals": [
+                    {"name": "load", "width": 1},
+                ],
+            },
+            {
+                "name": "a",
+                "descr": "operand a",
+                "signals": [
+                    {"name": "a", "width": 8},
+                ],
+            },
+            {
+                "name": "b",
+                "descr": "operand b",
+                "signals": [
+                    {"name": "b", "width": 8},
+                ],
+            },
+            {
+                "name": "mul1",
+                "descr": "Multiplier reg 1",
+                "signals": [
+                    {"name": "mul1", "width": 16},
+                ],
+            },
+            {
+                "name": "mul2",
+                "descr": "Multiplier reg 2",
+                "signals": [
+                    {"name": "mul2", "width": 16},
+                ],
+            },
+            {
+                "name": "acc",
+                "descr": "Accumulator",
+                "signals": [
+                    {"name": "acc", "width": 16},
+                ],
+            },
+            {
+                "name": "done",
+                "descr": "Done",
+                "signals": [
+                    {"name": "done", "width": 3},
+                ],
+            },
+            {
+                "name": "done_int",
+                "descr": "Done internal",
+                "signals": [
+                    {"name": "done_int", "width": 1},
+                ],
+            },
+        ],
+        "subblocks": [
+            # iob_csrs 'control_if_s' port is connected automatically by py2hwsw
+            f"""iob_csrs iob_csrs
+                -d 'Control/Status Registers' 
+                -c 
+                    "clk_en_rst_s":"clk_en_rst_s"
+                    "en_o":"en"
+                    "done_i":"done_int"
+                    "load_o":"load"
+                    "a_o":"a"
+                    "b_o":"b"
+                    "c_i":"acc"
+                --csr_if {CSR_IF}
+                --csr-group axil_macc 
+                    -d 'AXIL_MACC software accessible registers' 
+                        -r en:1 -t W -d 'Enable.'
+                        -r done:1 -t R -d 'Done.'
+                        -r load:1 -t W -d 'Load.'
+                        -r a:8 -t W -d 'Operand A.'
+                        -r b:8 -t W -d 'Operand B.'
+                        -r c:16 -t R -d 'Operand C.'
+            """,
+        ],
+        "superblocks": [
+            # Tester
+            {
+                "core_name": "iob_axil_macc_tester",
+                "dest_dir": "tester",
+            },
+            # Simulation wrapper
+            {
+                "core_name": "iob_axil_macc_sim",
+                "dest_dir": "hardware/simulation/src",
+                "csr_if": CSR_IF,
+            },
+        ],
+        "comb": {
+            "code": """
+            mul1_nxt = a*b;
+            mul2_nxt = mul1;
+            enp_nxt = en & ~enp;
+            acc_nxt = done[2]? (load? mul2: mul2+acc): acc;
+            done_nxt = {done[1], done[0], enp};
+            done_int_nxt = enp? 1'b0: (done[2] | done_int);
+            """,
+        },
+    }
+
+    return attributes_dict
---
+++ py2hwsw/lib/peripherals/iob_axil_macc/software/src/iob_core_tb.c
@@ -0,0 +1,50 @@
+/*
+ * SPDX-FileCopyrightText: 2025 IObundle
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "iob_axil_macc_csrs.h"
+#include <stdio.h>
+
+int iob_core_tb() {
+
+  // print welcome message
+  printf("IOB AXIL MACC testbench\n");
+
+  iob_axil_macc_csrs_set_load(1);
+  iob_axil_macc_csrs_set_a(1);
+  iob_axil_macc_csrs_set_b(2);
+
+  iob_axil_macc_csrs_set_en(1);
+  while (!iob_axil_macc_csrs_get_done())
+    ;
+  iob_axil_macc_csrs_set_en(0);
+  iob_axil_macc_csrs_set_load(0);
+
+  int i;
+
+  for (i = 0; i < 10; i++) {
+    iob_axil_macc_csrs_set_a(i + 3);
+    iob_axil_macc_csrs_set_b(i + 4);
+    iob_axil_macc_csrs_set_en(1);
+    while (!iob_axil_macc_csrs_get_done())
+      ;
+    iob_axil_macc_csrs_set_en(0);
+  }
+
+  int acc_hw = iob_axil_macc_csrs_get_done(0);
+
+  // compute in sw
+  int acc = 2;
+  for (i = 0; i < 10; i++)
+    acc += (i + 3) * (i + 4);
+
+  if (acc = acc_hw) {
+    printf("SW and HW computation match\n");
+    return 0;
+  } else {
+    printf("Error: SW and HW computation do not match\n");
+    return 1;
+  }
+}

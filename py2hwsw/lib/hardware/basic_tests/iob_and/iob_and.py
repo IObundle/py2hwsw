4c36ceaf170d286554cee28ca08f99c56ee3a27c
Artur Nóbrega <arturnobrega1999@gmail.com>
2025-06-27 04:43:43 +0100

    wip(api): Generate setters/getters automatically
    Revert part of previous commit.
    
    Current implementation idea:
    - API classes look like dataclasses and abstract classes:
      Their attributes are defined like class attributes, and their methods are abstract (no implementation).
    - 'api_for' decorator automatically creates getters/setters for each attribute of the API classes.
      It also deletes the original class attributes to prevent user from trying to access them directly.
      The generate getters/setter point to the values from the corresponding attributes in
      the internal class. Developers of Py2HWSW may implement their own
      getters/setters in the internal class for input validation.

--- py2hwsw/lib/hardware/basic_tests/iob_and/iob_and.py
+++ py2hwsw/lib/hardware/basic_tests/iob_and/iob_and.py
@@ -58,7 +58,7 @@ class iob_and(py2hwsw.iob_core):
 
 if __name__ == "__main__":
     conf_obj = py2hwsw.iob_conf(name="a")
-    print(conf_obj)
+    print(conf_obj.get_name())
     # iob_and_obj = iob_and()
     # print(iob_and_obj)
     # iob_and.generate_build_dir()

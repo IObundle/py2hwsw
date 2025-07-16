452dcdbf31a668f9c76be589821d65f3edfdc6b8
mbot27 <fimaales@gmail.com>
2025-07-16 19:54:26 +0100

    iob_gray2bin module converted to py2hwsw

--- py2hwsw/lib/hardware/fifo/iob_gray2bin/iob_gray2bin.py
+++ py2hwsw/lib/hardware/fifo/iob_gray2bin/iob_gray2bin.py
@@ -5,7 +5,56 @@
 
 def setup(py_params_dict):
     attributes_dict = {
-        "generate_hw": False,
+        "generate_hw": True,
+        "confs": [
+            {
+                "name": "DATA_W",
+                "descr": "Width",
+                "type": "P",
+                "val": "4",
+                "min": "NA",
+                "max": "NA",
+            },
+        ],
+        "ports": [
+            {
+                "name": "gr_i",
+                "descr": "Gray input",
+                "signals": [
+                    {
+                        "name": "gr_i",
+                        "width": "DATA_W",
+                        "descr": "Gray input signal",
+                    },
+                ],
+            },
+            {
+                "name": "bin_o",
+                "descr": "Binary output",
+                "signals": [
+                    {
+                        "name": "bin_o",
+                        "width": "DATA_W",
+                        "descr": "Binary output signal",
+                    },
+                ],
+            },
+        ],
+        "snippets": [
+            {
+                "verilog_code": f"""
+                genvar pos;
+
+                generate
+                   for (pos = 0; pos < DATA_W; pos = pos + 1) begin : gen_bin
+                      assign bin_o[pos] = ^gr_i[DATA_W-1:pos];
+                   end
+                endgenerate
+
+
+                """
+            }
+        ],
     }
 
     return attributes_dict

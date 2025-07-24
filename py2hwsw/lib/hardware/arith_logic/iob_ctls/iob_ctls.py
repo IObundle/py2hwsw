e82355d0674df7b8e63adcc59c5b9d92d40f0e1f
mbot27 <fimaales@gmail.com>
2025-07-24 17:37:14 +0100

    2#fix in iob_ctls

--- py2hwsw/lib/hardware/arith_logic/iob_ctls/iob_ctls.py
+++ py2hwsw/lib/hardware/arith_logic/iob_ctls/iob_ctls.py
@@ -59,39 +59,23 @@ def setup(py_params_dict):
         "wires": [
             {
                 "name": "data_int1",
-<<<<<<< HEAD
                 "descr": "Internal data",
-=======
-                "descr": "Potentially inverted data signal, based on SYMBOL parameter",
->>>>>>> cdd911ba0eeb9efca298e98fdf3834aebde65719
                 "signals": [
                     {
                         "name": "data_int1",
                         "width": "W",
-<<<<<<< HEAD
                         "descr": "Data internal wire",
-=======
-                        "descr": "Potentially inverted data signal derived from data_i",
->>>>>>> cdd911ba0eeb9efca298e98fdf3834aebde65719
                     },
                 ],    
             },
             {
                 "name": "data_int2",
-<<<<<<< HEAD
                 "descr": "Reversed data",
-=======
-                "descr": "Potentially reversed data signal",
->>>>>>> cdd911ba0eeb9efca298e98fdf3834aebde65719
                 "signals": [
                     {
                         "name": "data_int2",
                         "width": "W",
-<<<<<<< HEAD
                         "descr": "Reversed data internal wire",
-=======
-                        "descr": "Internal write enable signal",
->>>>>>> cdd911ba0eeb9efca298e98fdf3834aebde65719
                     },
                 ],
             },
@@ -118,11 +102,7 @@ def setup(py_params_dict):
         "snippets": [
             {
                 "verilog_code": r"""
-<<<<<<< HEAD
                // invert if searching zeros or not
-=======
-                // invert if searching zeros or not
->>>>>>> cdd911ba0eeb9efca298e98fdf3834aebde65719
                 generate
                    if (SYMBOL == 0) begin : g_zeros
                      assign data_int1 = data_i;

66502bb2379fc28ead540aca0af30e0955db89ad
mbot27 <fimaales@gmail.com>
2025-07-24 19:07:42 +0100

    module iob_diff converted to py2hwsw

--- py2hwsw/lib/hardware/arith_logic/iob_diff/iob_diff.py
+++ py2hwsw/lib/hardware/arith_logic/iob_diff/iob_diff.py
@@ -5,7 +5,70 @@
 
 def setup(py_params_dict):
     attributes_dict = {
-        "generate_hw": False,
+        "generate_hw": True,
+        "confs": [
+            {
+                "name": "DATA_W",
+                "descr": "Data width",
+                "type": "P",
+                "val": "32",
+                "min": "NA",
+                "max": "NA",
+            },
+            {
+                "name": "RST_VAL",
+                "descr": "Reset value",
+                "type": "P",
+                "val": "0",
+                "min": "NA",
+                "max": "NA",
+            },
+        ],
+        "ports": [
+            {
+                "name": "clk_en_rst_s",
+                "signals": {
+                    "type": "iob_clk",
+                    "params": "c_a_r",
+                },
+                "descr": "Clock, clock enable and reset",
+            },
+            {
+                "name": "data_i",
+                "descr": "Data input",
+                "signals": [
+                    {
+                        "name": "data_i",
+                        "width": "DATA_W",
+                        "descr": "Data input signal",
+                    },
+                ],
+            },
+            {
+                "name": "data_o",
+                "descr": "Data input",
+                "signals": [
+                    {
+                        "name": "data_o",
+                        "width": "DATA_W",
+                        "descr": "Data output signal",
+                    },
+                ],
+            },
+        ],
+        "wires": [
+            {
+                "name": "data_i_reg",
+                "descr": "Data input register wire",
+                "signals": [
+                    {
+                        "name": "data_i_reg",
+                        "width": "DATA_W",
+                        "descr": "Registered data input signal",
+                    },
+                ],
+            },
+        ],
         "subblocks": [
             {
                 "core_name": "iob_reg",
@@ -13,7 +76,23 @@ def setup(py_params_dict):
                 "port_params": {
                     "clk_en_rst_s": "c_a_r",
                 },
+                "parameters": {
+                    "DATA_W": "DATA_W",
+                    "RST_VAL": "RST_VAL",
+                },
+                "connect": {
+                    "clk_en_rst_s": "clk_en_rst_s",
+                    "data_i": "data_i",
+                    "data_o": "data_o",
+                },
             },
+        ],
+        "snippets": [
+            {
+                "verilog_code": r"""
+                assign data_o = data_i - data_i_reg;
+                """
+            }
         ],
     }
 

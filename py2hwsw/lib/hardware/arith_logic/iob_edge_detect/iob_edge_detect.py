beda0deae230de2a414aeb281f593019a894e8fa
mbot27 <fimaales@gmail.com>
2025-07-28 20:13:16 +0100

    module iob_edge_detect converted to py2hwsw

--- py2hwsw/lib/hardware/arith_logic/iob_edge_detect/iob_edge_detect.py
+++ py2hwsw/lib/hardware/arith_logic/iob_edge_detect/iob_edge_detect.py
@@ -5,21 +5,33 @@
 
 def setup(py_params_dict):
     attributes_dict = {
-        "generate_hw": False,
+        "generate_hw": True,
+        "confs": [
+            {
+                "name": "EDGE_TYPE",
+                "descr": "Edge detection type",
+                "type": "P",
+                "val": '"rising"',
+                "min": "NA",
+                "max": "NA",
+            },
+            {
+                "name": "OUT_TYPE",
+                "descr": "Output type",
+                "type": "P",
+                "val": '"step"',
+                "min": "NA",
+                "max": "NA",
+            },
+        ],
         "ports": [
             {
                 "name": "clk_en_rst_s",
                 "signals": {
                     "type": "iob_clk",
+                    "params": "c_a_r",
                 },
                 "descr": "Clock, clock enable and reset",
-            },
-            {
-                "name": "rst_i",
-                "descr": "",
-                "signals": [
-                    {"name": "rst_i", "width": 1},
-                ],
             },
             {
                 "name": "bit_i",
@@ -36,14 +48,81 @@ def setup(py_params_dict):
                 ],
             },
         ],
+        "wires": [
+            {
+                "name": "bit_int",
+                "descr": "Internal bit wire",
+                "signals": [
+                    {"name": "bit_int", "width": "1", "descr": "Internal bit signal"},
+                ],
+            },
+            {
+                "name": "bit_int_q",
+                "descr": "Internal bit wire with delay",
+                "signals": [
+                    {
+                        "name": "bit_int_q",
+                        "width": "1",
+                        "descr": "Internal bit signal with delay",
+                    },
+                ],
+            },
+        ],
         "subblocks": [
             {
                 "core_name": "iob_reg",
-                "instance_name": "iob_reg_r_inst",
+                "instantiate": False,
                 "port_params": {
                     "clk_en_rst_s": "c_a_r",
                 },
             },
+        ],
+        "snippets": [
+            {
+                "verilog_code": r"""
+                generate
+    if (EDGE_TYPE == "falling") begin : gen_falling
+      assign bit_int = ~bit_i;
+      iob_reg_car #(
+          .DATA_W (1),
+          .RST_VAL(1'b0)
+      ) bit_reg (
+          `include "iob_edge_detect_iob_clk_s_s_portmap.vs"
+          .data_i(bit_int),
+          .data_o(bit_int_q)
+      );
+
+    end else begin : gen_default_rising
+      assign bit_int = bit_i;
+
+      iob_reg_car #(
+          .DATA_W (1),
+          .RST_VAL(1'b1)
+      ) bit_reg (
+          `include "iob_edge_detect_iob_clk_s_s_portmap.vs"
+          .data_i(bit_int),
+          .data_o(bit_int_q)
+      );
+    end
+  endgenerate
+
+  generate
+    if (OUT_TYPE == "pulse") begin : gen_pulse
+      assign detected_o = bit_int & ~bit_int_q;
+    end else begin : gen_step
+      wire detected_prev;
+      iob_reg_car #(
+          .DATA_W(1)
+      ) detected_reg (
+          `include "iob_edge_detect_iob_clk_s_s_portmap.vs"
+          .data_i(detected_o),
+          .data_o(detected_prev)
+      );
+      assign detected_o = detected_prev | (bit_int & ~bit_int_q);
+    end
+  endgenerate
+                """
+            }
         ],
     }
 

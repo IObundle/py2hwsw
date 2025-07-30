d0392f5f016e3acba3757de46f0c9d581de8921b
Artur Nóbrega <arturnobrega1999@gmail.com>
2025-07-30 01:45:57 +0100

    feat(tb): Use named pipe for c2v.txt (more efficient)
    By using named pipe for the c2v.txt communication, the testbench no
    longer needs to poll the file. It can just make a blocking request to
    read data from the pipe. This is more efficient than wasting CPU time by
    polling the file.
    
    This implementation is an extension of solution proposed in
    PR https://github.com/IObundle/py2hwsw/pull/370.
    It has the same caveats (the iob_core_tb.c must make requests to the
    testbench for the simulation to advance).

--- py2hwsw/hardware/simulation/src/iob_v_tb.v
+++ py2hwsw/hardware/simulation/src/iob_v_tb.v
@@ -43,6 +43,7 @@ module iob_v_tb;
 
    // Variables
    integer req = -100, ack = 0, mode = -100, address = -100, data = -100, data_w = -100;
+   reg [8*45-1:0] buffer;  // array to hold 45 characters
 
    // Example test sequence (replace with your actual test logic)
    initial begin
@@ -65,28 +66,25 @@ module iob_v_tb;
       #10;
 
       // Open IPC files
-      c2v_read_fp = $fopen("c2v.txt", "rb");
-      if (c2v_read_fp == 0) begin
-         $display("Error: Could not open c2v.txt");
-         $finish;
+      while (c2v_read_fp == 0) begin
+         c2v_read_fp = $fopen("c2v.txt", "rb");
       end
-      $fclose(c2v_read_fp);
       v2c_write_fp = $fopen("v2c.txt", "wb");
       if (v2c_write_fp == 0) begin
-         $display("Error: Could not open v2c.txt");
+         $display("V: Error: Could not open v2c.txt");
          $finish;
       end
 
       // Server loop
       while (1) begin
          //read request
-         c2v_read_fp = $fopen("c2v.txt", "rb");
-         if ($fscanf(
-                 c2v_read_fp, "%08x %08x %08x %08x %08x\n", req, mode, address, data_w, data
-             )) begin
-            $fclose(c2v_read_fp);
-            /*$display("V: req=%08x mode=%08x address=%08x data_w=%08x data=%08x", req, mode,
-                     address, data_w, data);*/  // DEBUG
+         if ($fread(buffer, c2v_read_fp, 0, 45) != 45) begin
+            $display("V: Error: did not read expected 45 bytes from c2v.txt");
+            $finish;
+         end
+         if ($sscanf(buffer, "%08x %08x %08x %08x %08x\n", req, mode, address, data_w, data)) begin
+            /* $display("V: req=%08x mode=%08x address=%08x data_w=%08x data=%08x", req, mode,
+                     address, data_w, data); */  // DEBUG
             //check if request number matches with ack number
             if (req == ack) begin
                //process request
@@ -100,22 +98,23 @@ module iob_v_tb;
                   $fdisplay(v2c_write_fp, "%08x %08x %08x %08x %08x", ack, mode, address, data_w,
                             data);
                   $fflush(v2c_write_fp);
-                  //$display("V: read request: ack=%d adress=%08x data=%08x", ack, address, data); // DEBUG
+                  /* $display("V: read request: ack=%d adress=%08x data=%08x", ack, address,
+                           data); */  // DEBUG
                end else if (mode == `W) begin  //write request
                   iob_write(address, data, data_w);
                   //send ack
                   $fdisplay(v2c_write_fp, "%08x %08x %08x %08x %08x", ack, mode, address, data_w,
                             data);
                   $fflush(v2c_write_fp);
-                  // $display("V: write request: ack=%d adress=%08x data=%08x", ack, address, data); // DEBUG
+                  /* $display("V: write request: ack=%d adress=%08x data=%08x", ack, address,
+                           data); */  // DEBUG
                end
                ack = ack + 1;
             end  // if (req == ack)
-         end else begin  // if (fscanf_ret == 5)
-            $fclose(c2v_read_fp);
-         end  // if (fscanf_ret != 5)
+         end
       end  // while (1)
       $fclose(v2c_write_fp);
+      $fclose(c2v_read_fp);
    end  // initial begin
 
 

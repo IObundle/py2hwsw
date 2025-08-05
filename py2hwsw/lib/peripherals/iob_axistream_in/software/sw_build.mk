ecba01cc1f599e1e3ebbe01b4bde04a5a0fdb105
Artur Nóbrega <arturnobrega1999@gmail.com>
2025-08-05 22:24:44 +0100

    fix(verilator): Rename makefile variables; Fix iob_system(_linux).

--- py2hwsw/lib/peripherals/iob_axistream_in/software/sw_build.mk
+++ py2hwsw/lib/peripherals/iob_axistream_in/software/sw_build.mk
@@ -4,4 +4,4 @@
 
 UTARGETS=tb
 TB_INCLUDES=-I./src -I./simulation/src
-CSRS=./src/iob_axistream_in_csrs.c ./simulation/src/iob_axistream_out_csrs.c
+TB_SRC=./src/iob_axistream_in_csrs.c ./simulation/src/iob_axistream_out_csrs.c

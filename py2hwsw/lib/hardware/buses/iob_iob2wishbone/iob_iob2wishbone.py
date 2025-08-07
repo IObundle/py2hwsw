IObundle/py2hwsw

commit f7d33009e8e914f9b89c72a02cd197917d02599f

feat(wb): Generate iob2wishbone with py2hwsw; Fix bugs; Remove unused code.

py2hwsw/lib/hardware/buses/iob_iob2wishbone/iob_iob2wishbone.py
@@ -5,7 +5,39 @@
 
 def setup(py_params_dict):
     attributes_dict = {
-        "generate_hw": False,
+        "generate_hw": True,
+        #
+        # Confs
+        #
+        "confs": [
+            {
+                "name": "DATA_W",
+                "descr": "Data bus width",
+                "type": "P",
+                "val": "32",
+                "min": "NA",
+                "max": "NA",
+            },
+            {
+                "name": "ADDR_W",
+                "descr": "ADDR width",
+                "type": "P",
+                "val": "32",
+                "min": "NA",
+                "max": "NA",
+            },
+            {
+                "name": "READ_BYTES",
+                "descr": "",
+                "type": "P",
+                "val": "4",
+                "min": "NA",
+                "max": "NA",
+            },
+        ],
+        #
+        # Ports
+        #
         "ports": [
             {
                 "name": "clk_en_rst_s",
@@ -14,15 +46,296 @@ def setup(py_params_dict):
                 },
                 "descr": "Clock, clock enable and reset",
             },
-        ],
-        "subblocks": [
             {
-                "core_name": "iob_reg",
-                "port_params": {
-                    "clk_en_rst_s": "cke_arst_rst_en",
+                "name": "iob_s",
+                "signals": {
+                    "type": "iob",
+                    "ADDR_W": "ADDR_W",
+                    "DATA_W": "DATA_W",
                 },
+                "descr": "IOb native subordinate interface",
+            },
+            {
+                "name": "wb_m",
+                "signals": {
+                    "type": "wb",
+                    "ADDR_W": "ADDR_W",
+                    "DATA_W": "DATA_W",
+                },
+                "descr": "Wishbone manager interface",
+            },
+        ],
+        #
+        # Wires
+        #
+        "wires": [
+            # Reg valid
+            {
+                "name": "valid_data_i",
+                "descr": "valid intput wire",
+                "signals": [
+                    {"name": "iob_valid_i"},
+                ],
+            },
+            {
+                "name": "valid_data_o",
+                "descr": "valid output wire",
+                "signals": [
+                    {"name": "iob_valid_r", "width": 1},
+                ],
+            },
+            # Reg addr
+            {
+                "name": "addr_data_i",
+                "descr": "addr intput wire",
+                "signals": [
+                    {"name": "iob_addr_i"},
+                ],
+            },
+            {
+                "name": "addr_data_o",
+                "descr": "addr output wire",
+                "signals": [
+                    {"name": "iob_address_r", "width": "ADDR_W"},
+                ],
+            },
+            # Reg data
+            {
+                "name": "data_data_i",
+                "descr": "data intput wire",
+                "signals": [
+                    {"name": "iob_wdata_i"},
+                ],
+            },
+            {
+                "name": "data_data_o",
+                "descr": "data output wire",
+                "signals": [
+                    {"name": "iob_wdata_r", "width": "DATA_W"},
+                ],
+            },
+            # Reg we
+            {
+                "name": "we_data_i",
+                "descr": "we intput wire",
+                "signals": [
+                    {"name": "wb_we", "width": 1},
+                ],
+            },
+            {
+                "name": "we_data_o",
+                "descr": "we output wire",
+                "signals": [
+                    {"name": "wb_we_r", "width": 1},
+                ],
+            },
+            # Reg strb
+            {
+                "name": "strb_data_i",
+                "descr": "strb intput wire",
+                "signals": [
+                    {"name": "wb_select", "width": "DATA_W/8"},
+                ],
+            },
+            {
+                "name": "strb_data_o",
+                "descr": "strb output wire",
+                "signals": [
+                    {"name": "wb_select_r", "width": "DATA_W/8"},
+                ],
+            },
+            # Reg wb_data
+            {
+                "name": "wb_data_data_i",
+                "descr": "wb_data intput wire",
+                "signals": [
+                    {"name": "wb_dat_i"},
+                ],
+            },
+            {
+                "name": "wb_data_data_o",
+                "descr": "wb_data output wire",
+                "signals": [
+                    {"name": "wb_data_r", "width": "DATA_W"},
+                ],
+            },
+            # Reg wb_ack
+            {
+                "name": "wb_ack_data_i",
+                "descr": "wb_ack intput wire",
+                "signals": [
+                    {"name": "wb_ack_i"},
+                ],
+            },
+            {
+                "name": "wb_ack_data_o",
+                "descr": "wb_ack output wire",
+                "signals": [
+                    {"name": "wb_ack_r", "width": 1},
+                ],
             },
         ],
     }
+    #
+    # Subblocks
+    #
+    attributes_dict["subblocks"] = [
+        {
+            "core_name": "iob_reg",
+            "instance_name": "iob_reg_valid",
+            "parameters": {
+                "DATA_W": 1,
+                "RST_VAL": 0,
+            },
+            "port_params": {
+                "clk_en_rst_s": "cke_arst_rst_en",
+            },
+            "connect": {
+                "clk_en_rst_s": (
+                    "clk_en_rst_s",
+                    [
+                        "en_i:iob_valid_i",
+                        "rst_i:wb_ack_i",
+                    ],
+                ),
+                "data_i": "valid_data_i",
+                "data_o": "valid_data_o",
+            },
+        },
+        {
+            "core_name": "iob_reg",
+            "instance_name": "iob_reg_addr",
+            "parameters": {
+                "DATA_W": "ADDR_W",
+                "RST_VAL": 0,
+            },
+            "port_params": {
+                "clk_en_rst_s": "cke_arst_en",
+            },
+            "connect": {
+                "clk_en_rst_s": (
+                    "clk_en_rst_s",
+                    [
+                        "en_i:iob_valid_i",
+                    ],
+                ),
+                "data_i": "addr_data_i",
+                "data_o": "addr_data_o",
+            },
+        },
+        {
+            "core_name": "iob_reg",
+            "instance_name": "iob_reg_data",
+            "parameters": {
+                "DATA_W": "DATA_W",
+                "RST_VAL": 0,
+            },
+            "port_params": {
+                "clk_en_rst_s": "cke_arst_en",
+            },
+            "connect": {
+                "clk_en_rst_s": (
+                    "clk_en_rst_s",
+                    [
+                        "en_i:iob_valid_i",
+                    ],
+                ),
+                "data_i": "data_data_i",
+                "data_o": "data_data_o",
+            },
+        },
+        {
+            "core_name": "iob_reg",
+            "instance_name": "iob_reg_we",
+            "parameters": {
+                "DATA_W": 1,
+                "RST_VAL": 0,
+            },
+            "port_params": {
+                "clk_en_rst_s": "cke_arst_en",
+            },
+            "connect": {
+                "clk_en_rst_s": (
+                    "clk_en_rst_s",
+                    [
+                        "en_i:iob_valid_i",
+                    ],
+                ),
+                "data_i": "we_data_i",
+                "data_o": "we_data_o",
+            },
+        },
+        {
+            "core_name": "iob_reg",
+            "instance_name": "iob_reg_strb",
+            "parameters": {
+                "DATA_W": "DATA_W/8",
+                "RST_VAL": 0,
+            },
+            "port_params": {
+                "clk_en_rst_s": "cke_arst_en",
+            },
+            "connect": {
+                "clk_en_rst_s": (
+                    "clk_en_rst_s",
+                    [
+                        "en_i:iob_valid_i",
+                    ],
+                ),
+                "data_i": "strb_data_i",
+                "data_o": "strb_data_o",
+            },
+        },
+        {
+            "core_name": "iob_reg",
+            "instance_name": "iob_reg_wb_data",
+            "parameters": {
+                "DATA_W": "DATA_W",
+                "RST_VAL": 0,
+            },
+            "connect": {
+                "clk_en_rst_s": "clk_en_rst_s",
+                "data_i": "wb_data_data_i",
+                "data_o": "wb_data_data_o",
+            },
+        },
+        {
+            "core_name": "iob_reg",
+            "instance_name": "iob_reg_wb_ack",
+            "parameters": {
+                "DATA_W": 1,
+                "RST_VAL": 0,
+            },
+            "connect": {
+                "clk_en_rst_s": "clk_en_rst_s",
+                "data_i": "wb_ack_data_i",
+                "data_o": "wb_ack_data_o",
+            },
+        },
+    ]
+    #
+    # Snippets
+    #
+    attributes_dict["snippets"] = [
+        {
+            "verilog_code": """
+   // Logic
+   assign wb_adr_o     = iob_valid_i ? iob_addr_i : iob_address_r;
+   assign wb_datout_o  = iob_valid_i ? iob_wdata_i : iob_wdata_r;
+   assign wb_sel_o  = iob_valid_i ? wb_select : wb_select_r;
+   assign wb_we_o      = iob_valid_i ? wb_we : wb_we_r;
+   assign wb_cyc_o     = iob_valid_i ? iob_valid_i : iob_valid_r;
+   assign wb_stb_o     = wb_cyc_o;
+
+   assign wb_select    = wb_we ? iob_wstrb_i : {READ_BYTES{1'b1}};
+   assign wb_we        = |iob_wstrb_i;
+
+   assign iob_rvalid_o = wb_ack_r & (~wb_we_r);
+   assign iob_rdata_o  = wb_ack_i ? wb_dat_i : wb_data_r;
+   assign iob_ready_o  = (~iob_valid_r) | wb_ack_r;
+   // TODO: iob_rready
+""",
+        },
+    ]
 
     return attributes_dict

py2hwsw/lib/hardware/buses/iob_iob2wishbone/hardware/src/iob_iob2wishbone.v
@@ -1,137 +1,0 @@
-// SPDX-FileCopyrightText: 2025 IObundle
-//
-// SPDX-License-Identifier: MIT
-
-`timescale 1ns / 1ps
-
-module iob_iob2wishbone #(
-   parameter ADDR_W     = 32,
-   parameter DATA_W     = 32,
-   parameter READ_BYTES = 4
-) (
-   `include "iob_iob2wishbone_iob_clk_s_port.vs"
-
-   // IOb interface
-   input  wire                iob_valid_i,
-   input  wire [  ADDR_W-1:0] iob_addr_i,
-   input  wire [  DATA_W-1:0] iob_wdata_i,
-   input  wire [DATA_W/8-1:0] iob_wstrb_i,
-   output wire                iob_rvalid_o,
-   output wire [  DATA_W-1:0] iob_rdata_o,
-   output wire                iob_ready_o,
-   input  wire                iob_rready_i,
-
-   // Wishbone interface
-   output wire [  ADDR_W-1:0] wb_addr_o,
-   output wire [DATA_W/8-1:0] wb_select_o,
-   output wire                wb_we_o,
-   output wire                wb_cyc_o,
-   output wire                wb_stb_o,
-   output wire [  DATA_W-1:0] wb_data_o,
-   input  wire                wb_ack_i,
-   input  wire [  DATA_W-1:0] wb_data_i
-);
-
-   localparam RB_MASK = {1'b0, {READ_BYTES{1'b1}}};
-
-   // IOb auxiliar wires
-   wire                iob_valid_r;
-   wire [  ADDR_W-1:0] iob_address_r;
-   wire [  DATA_W-1:0] iob_wdata_r;
-   // Wishbone auxiliar wire
-   wire [  DATA_W-1:0] wb_data_r;
-   wire [DATA_W/8-1:0] wb_select;
-   wire [DATA_W/8-1:0] wb_select_r;
-   wire                wb_we;
-   wire                wb_we_r;
-   wire                wb_ack_r;
-
-   // Logic
-   assign wb_addr_o    = iob_valid_i ? iob_addr_i : iob_address_r;
-   assign wb_data_o    = iob_valid_i ? iob_wdata_i : iob_wdata_r;
-   assign wb_select_o  = iob_valid_i ? wb_select : wb_select_r;
-   assign wb_we_o      = iob_valid_i ? wb_we : wb_we_r;
-   assign wb_cyc_o     = iob_valid_i ? iob_valid_i : iob_valid_r;
-   assign wb_stb_o     = wb_cyc_o;
-
-   assign wb_select    = wb_we ? iob_wstrb_i : (RB_MASK) << (iob_addr_i[1:0]);
-   assign wb_we        = |iob_wstrb_i;
-
-   assign iob_rvalid_o = wb_ack_r & (~wb_we_r);
-   assign iob_rdata_o  = wb_ack_i ? wb_data_i : wb_data_r;
-   assign iob_ready_o  = (~iob_valid_r) | wb_ack_r;
-   // TODO: iob_rready
-
-   iob_reg_cear_re #(
-      .DATA_W (1),
-      .RST_VAL(0)
-   ) iob_reg_valid (
-      `include "iob_iob2wishbone_iob_clk_s_s_portmap.vs"
-      .rst_i (wb_ack_i),
-      .en_i  (iob_valid_i),
-      .data_i(iob_valid_i),
-      .data_o(iob_valid_r)
-   );
-   iob_reg_cear_re #(
-      .DATA_W (ADDR_W),
-      .RST_VAL(0)
-   ) iob_reg_addr (
-      `include "iob_iob2wishbone_iob_clk_s_s_portmap.vs"
-      .rst_i (1'b0),
-      .en_i  (iob_valid_i),
-      .data_i(iob_addr_i),
-      .data_o(iob_address_r)
-   );
-   iob_reg_cear_re #(
-      .DATA_W (DATA_W),
-      .RST_VAL(0)
-   ) iob_reg_iob_data (
-      `include "iob_iob2wishbone_iob_clk_s_s_portmap.vs"
-      .rst_i (1'b0),
-      .en_i  (iob_valid_i),
-      .data_i(iob_wdata_i),
-      .data_o(iob_wdata_r)
-   );
-   iob_reg_cear_re #(
-      .DATA_W (1),
-      .RST_VAL(0)
-   ) iob_reg_we (
-      `include "iob_iob2wishbone_iob_clk_s_s_portmap.vs"
-      .rst_i (1'b0),
-      .en_i  (iob_valid_i),
-      .data_i(wb_we),
-      .data_o(wb_we_r)
-   );
-   iob_reg_cear_re #(
-      .DATA_W (DATA_W / 8),
-      .RST_VAL(0)
-   ) iob_reg_strb (
-      `include "iob_iob2wishbone_iob_clk_s_s_portmap.vs"
-      .rst_i (1'b0),
-      .en_i  (iob_valid_i),
-      .data_i(wb_select),
-      .data_o(wb_select_r)
-   );
-   iob_reg_cear_re #(
-      .DATA_W (DATA_W),
-      .RST_VAL(0)
-   ) iob_reg_wb_data (
-      `include "iob_iob2wishbone_iob_clk_s_s_portmap.vs"
-      .rst_i (1'b0),
-      .en_i  (1'b1),
-      .data_i(wb_data_i),
-      .data_o(wb_data_r)
-   );
-   iob_reg_cear_re #(
-      .DATA_W (1),
-      .RST_VAL(0)
-   ) iob_reg_wb_ack (
-      `include "iob_iob2wishbone_iob_clk_s_s_portmap.vs"
-      .rst_i (1'b0),
-      .en_i  (1'b1),
-      .data_i(wb_ack_i),
-      .data_o(wb_ack_r)
-   );
-
-
-endmodule

py2hwsw/lib/hardware/buses/iob_wishbone2iob/iob_wishbone2iob.py
@@ -53,10 +53,10 @@ def setup(py_params_dict):
         ],
         "wires": [
             {
-                "name": "valid",
-                "descr": "valid wire",
+                "name": "valid_int",
+                "descr": "valid_int wire",
                 "signals": [
-                    {"name": "valid", "width": 1},
+                    {"name": "valid_int", "width": 1},
                 ],
             },
             {
@@ -81,13 +81,6 @@ def setup(py_params_dict):
                 ],
             },
             {
-                "name": "rdata_r",
-                "descr": "rdata_r wire",
-                "signals": [
-                    {"name": "rdata_r", "width": "DATA_W"},
-                ],
-            },
-            {
                 "name": "wack",
                 "descr": "wack wire",
                 "signals": [
@@ -102,54 +95,10 @@ def setup(py_params_dict):
                 ],
             },
             {
-                "name": "wb_addr_r",
-                "descr": "wb_addr_r wire",
-                "signals": [
-                    {"name": "wb_addr_r", "width": "ADDR_W"},
-                ],
-            },
-            {
-                "name": "wb_data_r",
-                "descr": "wb_data_r wire",
-                "signals": [
-                    {"name": "wb_data_r", "width": "DATA_W"},
-                ],
-            },
-            {
                 "name": "wb_data_mask",
                 "descr": "wb_data_mask wire",
                 "signals": [
                     {"name": "wb_data_mask", "width": "DATA_W"},
-                ],
-            },
-            {
-                "name": "reg_wack_int",
-                "descr": "reg_wack_int wire",
-                "signals": [
-                    {"name": "reg_wack_int", "width": 1},
-                ],
-            },
-            {
-                "name": "reg_wack_int_1",
-                "descr": "reg_wack_int_1 wire",
-                "signals": [
-                    {"name": "reg_wack_int_1", "width": 1},
-                ],
-            },
-            {
-                "name": "int",
-                "descr": "int wire",
-                "signals": [
-                    {"name": "reg_wack_int_1"},
-                    {"name": "reg_wack_int"},
-                ],
-            },
-            {
-                "name": "int_2",
-                "descr": "int wire",
-                "signals": [
-                    {"name": "valid"},
-                    {"name": "rst_valid"},
                 ],
             },
         ],
@@ -161,17 +110,8 @@ def setup(py_params_dict):
                     "DATA_W": 1,
                     "RST_VAL": 0,
                 },
-                "port_params": {
-                    "clk_en_rst_s": "cke_arst_rst_en",
-                },
                 "connect": {
-                    "clk_en_rst_s": (
-                        "clk_en_rst_s",
-                        [
-                            "en_i:reg_wack_int_1",
-                            "rst_i:reg_wack_int",
-                        ],
-                    ),
+                    "clk_en_rst_s": "clk_en_rst_s",
                     "data_i": "wack",
                     "data_o": "wack_r",
                 },
@@ -190,11 +130,11 @@ def setup(py_params_dict):
                     "clk_en_rst_s": (
                         "clk_en_rst_s",
                         [
-                            "en_i:valid",
+                            "en_i:valid_int",
                             "rst_i:rst_valid",
                         ],
                     ),
-                    "data_i": "valid",
+                    "data_i": "valid_int",
                     "data_o": "valid_r",
                 },
             },
@@ -202,21 +142,19 @@ def setup(py_params_dict):
         "snippets": [
             {
                 "verilog_code": """
-             assign iob_valid_o = valid;
-        assign iob_address_o = wb_adr_i;
-        assign iob_wdata_o = wb_dat_i;
-        assign iob_wstrb_o = wstrb;
-        assign valid = (wb_stb_i & wb_cyc_i) & (~valid_r);
-        assign rst_valid = (~wb_stb_i) & valid_r;
-        assign wstrb = wb_we_i ? wb_sel_i : 4'h0;
-        assign wb_dat_o = (iob_rdata_i) & (wb_data_mask);
-        assign wb_ack_o = iob_rvalid_i | wack_r;
-        assign wack = iob_ready_i & iob_valid_o & (|iob_wstrb_o);
-        assign wb_data_mask = {{8{wb_sel_i[3]}}, {8{wb_sel_i[2]}}, {8{wb_sel_i[1]}}, {8{wb_sel_i[0]}}};
-        assign reg_wack_int= 1'b0; 
-        assign reg_wack_int_1= 1'b1;   
-        assign iob_rready = 1'b1;
-                """,
+   assign iob_valid_o = valid_int;
+   assign iob_addr_o = wb_adr_i;
+   assign iob_wdata_o = wb_datout_i;
+   assign iob_wstrb_o = wstrb;
+   assign valid_int = (wb_stb_i & wb_cyc_i) & (~valid_r);
+   assign rst_valid = (~wb_stb_i) & valid_r;
+   assign wstrb = wb_we_i ? wb_sel_i : 4'h0;
+   assign wb_dat_o = (iob_rdata_i) & (wb_data_mask);
+   assign wb_ack_o = iob_rvalid_i | wack_r;
+   assign wack = iob_ready_i & iob_valid_o & (|iob_wstrb_o);
+   assign wb_data_mask = {{8{wb_sel_i[3]}}, {8{wb_sel_i[2]}}, {8{wb_sel_i[1]}}, {8{wb_sel_i[0]}}};
+   assign iob_rready_o = 1'b1;
+""",
             },
         ],
     }

py2hwsw/lib/hardware/iob_csrs/scripts/csr_gen.py
@@ -811,8 +811,8 @@ class csr_gen:
                     "instance_name": "iob_wishbone2iob_coverter",
                     "instance_description": "Convert Wishbone port into internal IOb interface",
                     "parameters": {
-                        "APB_ADDR_W": "ADDR_W",
-                        "APB_DATA_W": "DATA_W",
+                        "ADDR_W": "ADDR_W",
+                        "DATA_W": "DATA_W",
                     },
                     "connect": {
                         "clk_en_rst_s": "clk_en_rst_s",

py2hwsw/lib/peripherals/iob_uart/hardware/simulation/iob_uart_sim/iob_uart_sim.py
@@ -90,12 +90,12 @@ def setup(py_params_dict):
                 "instance_name": "iob_iob2wishbone_coverter",
                 "instance_description": "Convert IOb port into Wishbone interface for uart CSRs bus",
                 "parameters": {
-                    "APB_ADDR_W": 3 - 2,
-                    "APB_DATA_W": "DATA_W",
+                    "ADDR_W": 3 - 2,
+                    "DATA_W": "DATA_W",
                 },
                 "connect": {
                     "clk_en_rst_s": "clk_en_rst_s",
-                    "apb_m": "uart_cbus",
+                    "wb_m": "uart_cbus",
                     "iob_s": (
                         "uart_s",
                         [

py2hwsw/scripts/doc_gen.py
@@ -204,11 +204,13 @@ def process_tex_macros(tex_src_dir):
 
 
 def process_tex_macro(line):
+    """Given a TeX macro line, return a new (multi)line with appropriate value."""
     macro = line.strip().split(":")[1].strip().split()
     macro_command = macro[0]
     listing_content = ""
 
     def _find_file(file):
+        """Local function to find file, and print error otherwise"""
         filename, extension = file.split(".")
         file = find_file(
             os.pwath.join(os.path.dirname(__file__), ".."),

py2hwsw/scripts/if_gen.py
@@ -992,7 +992,7 @@ def get_wb_ports():
             descr="Data input.",
         ),
         iob_signal(
-            name="wb_dat_o",
+            name="wb_datout_o",
             width=DATA_W,
             descr="Data output.",
         ),

commit 7b836781c2652892eaf990fbd07acd6f303f16c5

fix(iob_iob2wishbone.py): Fix bug in wb_select signal
Fix bug introduced conversion in commit https://github.com/IObundle/py2hwsw/commit/f7d33009e8e914f9b89c72a02cd197917d02599f#diff-23f4adcfd5e76b32f7d5603c1fcebc3e5eef8be5405ace7cf0cfc756a20956c4

The 'wb_select' should change according to the LSB of the address.

Simulation of 'iob_system_linux' is working with baremetal firmware.

py2hwsw/lib/hardware/buses/iob_iob2wishbone/iob_iob2wishbone.py
@@ -299,6 +299,8 @@ def setup(py_params_dict):
     attributes_dict["snippets"] = [
         {
             "verilog_code": """
+   localparam RB_MASK = {1'b0, {READ_BYTES{1'b1}}};
+
    // Logic
    assign wb_adr_o     = iob_valid_i ? iob_addr_i : iob_address_r;
    assign wb_cyc_o     = iob_valid_i ? iob_valid_i : iob_valid_r;
@@ -307,7 +309,7 @@ def setup(py_params_dict):
    assign wb_sel_o     = iob_valid_i ? wb_select : wb_select_r;
    assign wb_we_o      = iob_valid_i ? wb_we : wb_we_r;
 
-   assign wb_select    = wb_we ? iob_wstrb_i : {READ_BYTES{1'b1}};
+   assign wb_select    = wb_we ? iob_wstrb_i : (RB_MASK) << (iob_addr_i[1:0]);
    assign wb_we        = |iob_wstrb_i;
 
    assign iob_rvalid_o = wb_ack_i & (~wb_we_r);
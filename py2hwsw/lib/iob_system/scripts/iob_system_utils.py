e8994e6f0dcebb026479155915849bad29107b81
Artur Nóbrega <arturnobrega1999@gmail.com>
2025-08-21 16:42:09 +0100

    feat(eth): Don't create dummy network interface if it already exists
    Also, another tip to avoid creating pyRawWrapper every time, from the lib
    directory, run:
    ```
    make -C iob_system/submodules/iob_eth/scripts/pyRawWrapper all
    export IOB_CONSOLE_PYTHON_ENV=$(pwd)/iob_system/submodules/iob_eth/scripts/pyRawWrapper/pyRawWrapper
    ```
    The next time 'make sim-run CORE=iob_system USE_ETHERNET=1' is run, the existing pyRawWrapper will be used.

--- py2hwsw/lib/iob_system/scripts/iob_system_utils.py
+++ py2hwsw/lib/iob_system/scripts/iob_system_utils.py
@@ -481,6 +481,11 @@ $(PYRAWWRAPPER_PATH):
 PRIVILEGED_CMD = $(shell command -v doas > /dev/null 2>&1 && echo "doas" || command -v sudo > /dev/null 2>&1 && echo "sudo sh -c" || echo "su root -c")
 BUILD_DEPS+=virtual-network-if
 virtual-network-if:
+	@if ip link show $(ETH_IF) &> /dev/null; then \
+	    echo "Interface $(ETH_IF) exists, skipping interface creation"; \
+	    exit 0; \
+	fi
+	@echo "Creating dummy network interface: $(ETH_IF)"
 	$(PRIVILEGED_CMD) "modprobe dummy;\
 	ip link add $(ETH_IF) type dummy;\
 	ifconfig $(ETH_IF) up"

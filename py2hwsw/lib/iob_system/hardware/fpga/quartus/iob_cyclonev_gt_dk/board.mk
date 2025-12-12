1c500ff0012bda80e401e502e2c53bf012223659
P-Miranda <pmiranda.iobundle@gmail.com>
2025-12-12 18:38:20 +0000

    fix(cyclonev): add iob_reset_sync if exists only

--- py2hwsw/lib/iob_system/hardware/fpga/quartus/iob_cyclonev_gt_dk/board.mk
+++ py2hwsw/lib/iob_system/hardware/fpga/quartus/iob_cyclonev_gt_dk/board.mk
@@ -6,7 +6,9 @@ BOARD_SERVER=$(CYC5_SERVER)
 BOARD_USER=$(CYC5_USER)
 BOARD_SERIAL_PORT=$(CYC5_SERIAL_PORT)
 
+ifneq ($(wildcard $(FPGA_TOOL)/$(BOARD)/iob_reset_sync.v),)
 VSRC+=$(FPGA_TOOL)/$(BOARD)/iob_reset_sync.v
+endif
 
 ifeq ($(USE_EXTMEM),1)
 VSRC+=$(FPGA_TOOL)/$(BOARD)/alt_ddr3.qsys

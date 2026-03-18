1592d7310f666fe132b8fda71bcc5c2b7351fee1
Artur Nóbrega <arturnobrega1999@gmail.com>
2026-03-18 15:00:58 +0000

    feat(linux): Add +2 uart16550 instances to linux; Configure PLIC sources

--- py2hwsw/lib/iob_system/iob_system_linux/iob_system_linux.py
+++ py2hwsw/lib/iob_system/iob_system_linux/iob_system_linux.py
@@ -50,7 +50,7 @@ def setup(py_params: dict):
     # Verilog snippets
     #
     verilog_snippet = """
-   assign interrupts = {{30{1'b0}}, uart_interrupt, 1'b0};
+   assign interrupts = {{28{1'b0}}, uart2_interrupt, uart1_interrupt, uart0_interrupt, 1'b0};
 """
 
     if params["dma_demo"]:
@@ -113,12 +113,26 @@ def setup(py_params: dict):
         ],
         "wires": [
             # UART
+            {"name": "uart0_interrupt", "signals": [{"name": "uart0_interrupt", "width": 1}]},
+            {"name": "uart1_interrupt", "signals": [{"name": "uart1_interrupt", "width": 1}]},
+            {"name": "uart2_interrupt", "signals": [{"name": "uart2_interrupt", "width": 1}]},
             {
-                "name": "uart_interrupt",
-                "descr": "Uart interrupt",
-                "signals": [
-                    {"name": "uart_interrupt", "width": 1},
-                ],
+                "name": "internal_rs232",
+                "descr": "Internal RS232 connection between UART1 and UART2",
+                "signals": {
+                    "type": "rs232",
+                    "prefix": "internal_"
+                },
+            },
+            {
+                "name": "internal_rs232_inverted",
+                "descr": "Internal RS232 connection between UART1 and UART2",
+                "signals": {
+                    {"name": "internal_rs232_txd"},
+                    {"name": "internal_rs232_rxd"},
+                    {"name": "internal_rs232_cts"},
+                    {"name": "internal_rs232_rts"},
+                },
             },
             # SPI master
             # {
@@ -155,8 +169,39 @@ def setup(py_params: dict):
                     "clk_en_rst_s": "clk_en_rst_s",
                     # Cbus connected automatically
                     "rs232_m": "rs232_m",
-                    "interrupt_o": "uart_interrupt",
+                    "interrupt_o": "uart0_interrupt",
                 },
+                "plice_source_id": 1,
+            },
+            {
+                # Instantiate a UART16550 core from: https://github.com/IObundle/iob-uart16550
+                "core_name": "iob_uart16550",
+                "instance_name": "UART1",  # Use same name as one inherited from iob_system to replace it
+                "instance_description": "UART peripheral",
+                "is_peripheral": True,
+                "parameters": {},
+                "connect": {
+                    "clk_en_rst_s": "clk_en_rst_s",
+                    # Cbus connected automatically
+                    "rs232_m": "internal_rs232",
+                    "interrupt_o": "uart1_interrupt",
+                },
+                "plice_source_id": 2,
+            },
+            {
+                # Instantiate a UART16550 core from: https://github.com/IObundle/iob-uart16550
+                "core_name": "iob_uart16550",
+                "instance_name": "UART2",  # Use same name as one inherited from iob_system to replace it
+                "instance_description": "UART peripheral",
+                "is_peripheral": True,
+                "parameters": {},
+                "connect": {
+                    "clk_en_rst_s": "clk_en_rst_s",
+                    # Cbus connected automatically
+                    "rs232_m": "internal_rs232_inverted",
+                    "interrupt_o": "uart2_interrupt",
+                },
+                "plice_source_id": 3,
             },
             # {
             #     # Instantiate a VERSAT core from: https://github.com/IObundle/iob-versat

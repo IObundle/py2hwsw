b04ed7d4d78f802d485c7da2b42d4a8f3c31a9aa
Artur Nóbrega <arturnobrega1999@gmail.com>
2026-04-13 06:11:08 +0100

    fix(linux): Fix CLINT and PLIC demo test script

--- py2hwsw/lib/iob_system/iob_system_linux/hardware/fpga/clint_plic_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo === Interrupt Test ===
-echo CLINT Timer: $(cat /proc/interrupts | grep timer | awk '{print $2}')
-echo PLIC Contexts: $(dmesg | grep plic | grep contexts)
-echo Total IRQs: $(wc -l < /proc/interrupts)
-echo Uptime: $(uptime)
